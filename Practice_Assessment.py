e0c36ad58fb08f680d7f96e6f5a92bc7df6cacdb
toastisverygood <leonj.23075@obhs.school.nz>
2025-08-21 09:48:25 +1200

    added code commiting and finctiona and libaries

--- Practice_Assessment.py
+++ Practice_Assessment.py
@@ -1,14 +1,18 @@
-import random
+#-ibaries 
+import random #imports rnadome numbers 
 
 def heads_tails():
     pass
-
-
+#finctions--------
+#----------------------------
+#this function gets a rondom nuber to 
+#chose from a list of heads or tails like a coin flip
+#----------------------------
 
 #main----------
-print('Welcome to heads or tails game ')
-name = input('what is your name?')
-age = int(input(' what is your age '))
+print('Welcome to heads or tails game ') #greeting to the game 
+name = input('what is your name?') #asks for your name
+age = int(input(' what is your age ')) #asks for your age 
 
 
-heads_tails()
+heads_tails()#calling function with no peramoter
