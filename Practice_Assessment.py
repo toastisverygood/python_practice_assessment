15fc18baa2c95c593c3b666afc93fe8295e4cb53
toastisverygood <leonj.23075@obhs.school.nz>
2025-08-21 09:40:40 +1200

    inputs and finctions

--- Practice_Assessment.py
+++ Practice_Assessment.py
@@ -1,1 +1,14 @@
 import random
+
+def heads_tails():
+    pass
+
+
+
+#main----------
+print('Welcome to heads or tails game ')
+name = input('what is your name?')
+age = int(input(' what is your age '))
+
+
+heads_tails()
